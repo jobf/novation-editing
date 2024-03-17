24d4d8e80deddbfde70e82573bb1d04419956072
jf <22012032+jobf@users.noreply.github.com>
2024-03-17 15:36:50 +0000

    load nrpn parameter from pdf data

--- test/ParameterEditingTests.hx
+++ test/ParameterEditingTests.hx
@@ -3,9 +3,8 @@ import ParameterParsing;
 import utest.Assert;
 import utest.Test;
 
-class ParameterEditingTests extends Test{
-
-	function test_cc_parameter_from_pdf_data(){
+class ParameterEditingTests extends Test {
+	function test_cc_parameter_from_pdf_data() {
 		var pdf_data:PdfDataFormat = {
 			section: "Voice",
 			parameter: "Polyphony Mode",
@@ -26,5 +25,29 @@ class ParameterEditingTests extends Test{
 		Assert.same(2, parameter.maximum_value);
 		Assert.same(2, parameter.default_value);
 		Assert.same("0=Mono 1=Mono AG 2=Poly", parameter.description);
+	}
+	
+	function test_nrpn_parameter_from_pdf_data() {
+		var pdf_data:PdfDataFormat = {
+			section: "LFO",
+			parameter: "lfo 1 slew rate",
+			message_type: "NRPN",
+			control_number: "3:72",
+			range: "0:127",
+			default_value: "0",
+			notes: ""
+		}
+
+		var parameter = ParameterEdit.from_pdf_data(pdf_data);
+
+		Assert.same("LFO", parameter.section);
+		Assert.same("lfo 1 slew rate", parameter.name);
+		Assert.same(MessageType.NRPN, parameter.type);
+		Assert.same(3, parameter.control_number[0]);
+		Assert.same(72, parameter.control_number[1]);
+		Assert.same(0, parameter.minimum_value);
+		Assert.same(127, parameter.maximum_value);
+		Assert.same(0, parameter.default_value);
+		Assert.same("", parameter.description);
 	}
 }
