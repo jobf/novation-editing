75e89e362c9cf5804f6b31942d9adeb5c2b63160
jf <22012032+jobf@users.noreply.github.com>
2024-03-22 12:01:30 +0000

    add sysex patch data

--- test/ValidateCsvFileContents.hx
+++ test/ValidateCsvFileContents.hx
@@ -3,29 +3,58 @@ import utest.Assert;
 import utest.Test;
 
 class ValidateCsvFileContents extends Test {
-	var csv_file_contents:String;
+	var csv_parameters_content:String;
+	var csv_patch_content:String;
 
 	function setupClass() {
-		csv_file_contents = Resource.getString("CircuitParametersFromPdf");
+		csv_parameters_content = Resource.getString("CircuitParametersFromPdf");
+		csv_patch_content = Resource.getString("CircuitSysExPatchCsv");
 	}
 
-	function test_resource_is_present() {
-		Assert.isTrue(csv_file_contents.length > 0);
+	function test_resources_are_presen() {
+		Assert.isTrue(csv_parameters_content.length > 0);
+		Assert.isTrue(csv_patch_content.length > 0);
 	}
 
-	function test_can_load_csv_data() {
-		var csv_data = CSV.split_csv_data(csv_file_contents);
+	function test_can_load_csv_parameter_data() {
+		var csv_data = CSV.split_csv_data(csv_parameters_content);
 		Assert.isTrue(csv_data.length > 0);
 		Assert.isTrue(csv_data[0].length > 0);
 	}
 
-	function test_every_line_has_enough_columns() {
-		var csv_data = CSV.split_csv_data(csv_file_contents);
+	function test_can_load_csv_patch_data() {
+		var csv_data = CSV.split_csv_data(csv_patch_content);
+		Assert.isTrue(csv_data.length > 0);
+		Assert.isTrue(csv_data[0].length > 0);
+	}
+
+	function test_parameter_data_lines_have_enough_columns() {
+		var csv_data = CSV.split_csv_data(csv_parameters_content);
 
 		var invalid_line_indexes:Array<Int> = [];
 
 		for (index => row in csv_data) {
 			if (row.length < 7) {
+				invalid_line_indexes.push(index);
+			}
+		}
+
+		Assert.equals(0, invalid_line_indexes.length);
+
+		if(invalid_line_indexes.length > 0){
+			trace('check these lines:');
+			trace(invalid_line_indexes);
+		}
+	}
+
+
+	function test_patch_data_lines_have_enough_columns() {
+		var csv_data = CSV.split_csv_data(csv_patch_content);
+
+		var invalid_line_indexes:Array<Int> = [];
+
+		for (index => row in csv_data) {
+			if (row.length < 6) {
 				invalid_line_indexes.push(index);
 			}
 		}
